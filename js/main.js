0f504075905d90fee84e3d5c7af98982a68a58d9
Liz Hodgson <lizhodgson@gmail.com>
2017-03-02 12:12:42 -0500

    attempted to add masonry
    But it’s not working that well.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-main.js
+$(document).ready(function() {
+
+	// Initialize Masonry
+	$('#content').masonry({
+		columnWidth: 320,
+		itemSelector: '.item',
+		isFitWidth: true,
+		isAnimated: !Modernizr.csstransitions
+	}).imagesLoaded(function() {
+		$(this).masonry('reload');
+	});
+
+});
